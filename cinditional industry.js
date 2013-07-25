d673cbfc91bd7c098c79f77b53b3fb5b5903a8f9
tamara <tamarajones0082@gmail.com>
2013-07-25 18:43:49 -0500

    work 3

---
+++ cinditional industry.js
@@ -0,0 +1,50 @@
+// conditional industry 
+
+var colors  = true; 
+
+// colors are pretty in web design " then i use the colors!"
+if(colors){
+    //i use them colors!
+    console.log(than i use the colors!");
+
+
+var  colorsareprettyinwebdesign = 27;
+var  primarycolorarethere = 3;
+var  secondarycolorsthere = 3;
+
+//  color sare pretty in web design " then i use the colors!"
+//if the are 27 pretty colors 
+if(colors > 27){
+    //code performed if condition is true 
+    console.log("than i use the colors!");
+}else if(primarycolorarethere > i dont use the colors){
+    // i use the color
+    console.log("beautiful web page!");
+}else{
+    // no one like my design 
+    console.log("no one like my design!");
+}
+
+
+var  colorsareusedinwebdesign = 27;
+var  primarycolorarethere = 3;
+var   secondarycolorsthere = 3;
+
+// if the color used is less our the primary colors AND if our secondary colors is over 
+if( color used< primary colors && secondary colors > 27){
+   console.log("than i use the colors!");
+}else{
+   console.log("no one like my design!");
+   
+   
+var colorsareprettyinwebdesign = 27;
+//colors are used in web design, print the console " then i use the colors!"
+if(colorsareprettyinwebdesign > 27){
+  console.log("than i use the colors!");
+}else{
+console.log("no one like my design!");
+*/
+ 
+(colors > 27) ? console.log("than i use the colors!") : console.log("no one like my design!");
+
+
