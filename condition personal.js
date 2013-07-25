558514b83b14332950d10d4b79b69356e4a73cdb
tamara <tamarajones0082@gmail.com>
2013-07-25 17:24:06 -0500

    work 2

---
+++ condition personal.js
@@ -0,0 +1,50 @@
+// conditional personal 
+
+var schoollife  = true; 
+
+//if i paid attention in class, print the console " then i want fail!"
+if(schoollife){
+    //i want fail the class!
+    console.log(than i want fail the class!");
+
+
+var  Exgrades = 90;
+var  newgrade = 85;
+var  failing grade = 65;
+
+//if i play attention in class, print the console " then i want fail!"
+//if my grade is 85 than i passed
+if(grades > 85){
+    //code performed if condition is true 
+    console.log("than i want fail the class!");
+}else if(newgrade > failing grade){
+    //i pass the class
+    console.log("you pass the class!");
+}else{
+    // you have to take this class again
+    console.log("you didn't pass the class!");
+}
+
+
+var  Exgrades = 90;
+var  newgrade = 85;
+var  failing grade = 65;
+
+// if  my Exgrade is less our newgrade AND if our failing grade is over 
+if(Exgrade < newgrade && failing grade > 90){
+   console.log("than i want fail the class!");
+}else{
+   console.log("you didn't pass the class!");
+   
+   
+var Exgrades = 90;
+//if i play attention in class, print the console " then i want fail!"
+if(Exgrades > 90){
+  console.log("than i want fail the class!");
+}else{
+console.log("you didn't pass the class!");
+*/
+ 
+(colorsareued > 90) ? console.log("than i want fail the class!") : console.log("you didn't pass the class!");
+
+
