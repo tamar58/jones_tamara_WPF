c387935935eb007057c098951fb919866ea5d410
tamara <tamarajones0082@gmail.com>
2013-07-25 15:19:07 -0500

    work

---
+++ Expression personal.js
@@ -0,0 +1,44 @@
+//Expression = Personal Expression
+
+var friends= 8;
+var familymemeber = 4 - classmates ;
+console.log(friends);
+
+
+var friends = 8;
+var familymemeber= 4 - classmates;
+console.log(friends);
+
+
+var friends= [ 8,4,2]
+
+var total friends= [0] + [1] + [2];
+console.log(total);
+
+
+var friends= "8friends";
+var familymemeber= "4family members;
+var colors =   + "friends "  + familymemeber ;
+
+var a = "8";
+var b = "4";
+var result = a + b;
+console.log(friends);
+
+
+var friends= 8;
+var familymemeber= 4;
+var familymemeber = friends * classmates;
+classmates = classmates/2;
+console.log(howmanysecondarycolorsthere);
+
+
+prompt("friends", "familymemeber");
+var name = prompt("familymemeber");
+var name = prompt("familymemeber","friends" );
+alert("8 friends " + name + "\n 4 family");
+
+
+var a = 8;
+c = a + 4;
+console.log(a + 8);
---
+++ Function industry .js
@@ -0,0 +1,73 @@
+Function industry 
+
+console.log('-------- loops ---------');
+
+var a = 30 ;
+while { a > 20){
+	console.log(a + 'it will be eye catching');
+	a--;
+}
+
+
+console.log('------- do while loops ----------');
+
+
+var a = 30
+
+do{
+	console.log(a + 'it will be eye catching ');
+	c--;
+}
+while(a > 27);
+
+
+console.log('-------- for loops --------');
+
+
+for (var i = 30; i > 20; i--){
+	console.log(i + ' it will be eye catching')
+}
+
+
+
+function webpage(){
+	console.log("it will be eye catchings");
+}
+
+var colorareused = 30;
+
+function grades(){
+	var colorareused = 30;
+	var dislikecolor = 20;
+	var colorsilike = colorareused * dislikecolor ;
+	console.log(colorsilike);
+}
+
+webpage();
+webpage();
+webpage();
+
+function webpage(){
+ 	var colorsilike = colorsused * dislikecolor;
+ 	console.log(colorsilike);
+}
+
+calcArea(30, 27);
+
+function calcArea(colorareused, dislikecolor){
+	var colorsilike  = colorareused * dislikecolor;
+	console.log(colorsilike);
+}
+
+
+function colors(colorareused){
+	var colorareused = 30;
+	var dislikecolor = colorareused * 27
+	console.log("it will be eye catchings " + dislikecolor + colorsused" );
+}
+var colorareused1 = 30;
+dislikecolor(colorareused1);
+dislikecolor(27);
+	
+
+	
