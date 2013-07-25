a58d7f687709cbccc64aa6e55711aec76eb7d654
tamara <tamarajones0082@gmail.com>
2013-07-25 15:03:53 -0500

    setting up github

---
+++ Expression industry.js
@@ -0,0 +1,44 @@
+///Expression = industry
+
+var howmanycolorsareusedinwebdesign= 27;
+var howmanyprimarycolorarethere = 3 - howmanysecondarycolorsthere;
+console.log(howmanycolorsareusedinwebdesign);
+
+
+var howmanycolorsareusedinwebdesign = 27;
+var howmanyprimarycolorarethere = 3 - howmanysecondarycolorsarethere;
+console.log(howmanycolorsareusedinwebdesign);
+
+
+var howmanycolorsareusedinwebdesign  = [ 27,3,3]
+
+var total howmanycolorsareusedinwebdesign = [0] + [1] + [2];
+console.log(total);
+
+
+var howmanycolorsareusedinwebdesign = "27colors";
+var howmanyprimarycolorarethere = "3primary";
+var colors = howmanycolorsareusedinwebdesign  + " "  + howmanyprimarycolorarethere ;
+
+var a = "27";
+var b = "3";
+var result = a + b;
+console.log(howmanycolorsareusedinwebdesign);
+
+
+var howmanycolorsareusedinwebdesign  = 27;
+var howmanyprimarycolorarethere = 3;
+var howmanysecondarycolorsthere = howmanycolorsareusedinwebdesign * howmanyprimarycolorarethere;
+howmanysecondarycolorsthere = howmanysecondarycolorsthere/3;
+console.log(howmanysecondarycolorsthere);
+
+
+prompt("how many colors are used in web design", "how many primary colors are there");
+var name = prompt("how many secondary colors are there");
+var name = prompt("how many primary colors are there","how many colors are used in web design" );
+alert("27 color that many people use" + name + "\n 3 primary colors ");
+
+
+var a = 27;
+c = a + 3;
+console.log(a + 27);
