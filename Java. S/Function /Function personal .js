98bbd3c6531065d3afaef701e433270ded687206
tamara <tamarajones0082@gmail.com>
2013-08-01 22:09:35 -0500

    All my work

---
+++ Java. S/Function /Function personal .js
@@ -0,0 +1,73 @@
+Function personal 
+
+console.log('-------- loops ---------');
+
+var a = 90 ;
+while { a > 85){
+	console.log(a + ' if i will pass the class ');
+	a--;
+}
+
+
+console.log('------- do while loops ----------');
+
+
+var a = 90
+
+do{
+	console.log(a + ' if i will pass the class ');
+	c--;
+}
+while(a > 85);
+
+
+console.log('-------- for loops --------');
+
+
+for (var i = 90; i > 85; i--){
+	console.log(i + ' if i pass the class')
+}
+
+
+
+function classes(){
+	console.log("if i will pass the class");
+}
+
+var exgrade = 90;
+
+function grades(){
+	var exgrade = 90;
+	var newgrade = 85;
+	var badgrade = exgrade * newgarde;
+	console.log(badgrade);
+}
+
+classes();
+classes();
+classes();
+
+function classes(){
+ 	var badgrade = exg * ng;
+ 	console.log(badgrade);
+}
+
+calcArea(90, 85);
+
+function calcArea(exgrade,newgrade){
+	var badgrade = exgrade * newgrade;
+	console.log(badgrade);
+}
+
+
+function classgrade(exgrade){
+	var exgrade = 90;
+	var newgrade = exgrade * 85 
+	console.log(" if i pass the class " + newgrade + " exgrade);
+}
+var exgrade1 = 90;
+newgrade(exgrade1);
+newgrade(85);
+	
+
+	
